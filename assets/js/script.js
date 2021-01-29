dca4cf701b7131f0756b0a85398641be1a48ba29
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-01-28 21:53:31 -0600

    ftn to call park name, description, url, img and a note for Josh in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,45 @@ var nationalPark=[""];
 
 function parks(parks){
     $(".parks").empty()
-    var url="https://developer.nps.gov/api/v1/parks?parkCode"+parks+"acad&api_key="+APIKey;
+    //developer.nps.gov/api/v1
+    //curl -X GET "https://developer.nps.gov/api/v1/parks?q=Smoky%20Mountains" -H "accept: application/json"
+    var url="https://developer.nps.gov/api/v1/parks?stateCode="+parks+"&api_key="+APIKey;
+    //curl -X GET "https://developer.nps.gov/api/v1/parks?stateCode=FL" -H "accept: application/json"
     console.log(url)
+    $.ajax({
+        url: url,
+        method: "GET"
+    })
+    .then(function(response) 
+    {
+        for(var i=0 ;i<response.data.length;i++){
+            console.log(response.data[i].fullName)
+            console.log(response.data[i].description)
+            console.log(response.data[i].url)
+            console.log(response.data[i].images[0].url)
+            //.parkarea is where the dynamic data gets appended to
+        }
+        
+    });
 }
 
 function campground(){
-
+    //curl -X GET "https://developer.nps.gov/api/v1/campgrounds?parkCode=&stateCode=FL" -H "accept: application/json"
+    console.log(url)
+    $.ajax({
+        url: url,
+        method: "GET"
+    })
+    .then(function(response) 
+    {
+        for(var i=0 ;i<response.data.length;i++){
+            console.log(response.data[i].fullName)
+            console.log(response.data[i].description)
+            console.log(response.data[i].url)
+            console.log(response.data[i].images[0].url)
+            //.parkarea is where the dynamic data gets appended to
+        }
+        
+    });
 }
-console.log(campground);
+parks("FL")
