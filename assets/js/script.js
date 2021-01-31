d1db200d63c97aef1a7fbd2336e31d0d2b6a006f
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-01-31 11:03:23 -0600

    function for weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var APIKey="GbSqqMYxDn7M1nf2UQOTRhNleBM2l0zy6iiHUpcS";
 var nationalPark=[""];
+var APIKey2="eba38d5202ab062097c0e84b133dc98e";
+var cityArray=["";]
 
 function parks(parks){
     $(".parks").empty()
@@ -25,8 +27,9 @@ function parks(parks){
     });
 }
 
-function campground(){
+function campground(campground){
     //curl -X GET "https://developer.nps.gov/api/v1/campgrounds?parkCode=&stateCode=FL" -H "accept: application/json"
+    var url="https://developer.nps.gov/api/v1/campgrounds?stateCode="+campground+"&api_key="+APIKey;
     console.log(url)
     $.ajax({
         url: url,
@@ -35,13 +38,32 @@ function campground(){
     .then(function(response) 
     {
         for(var i=0 ;i<response.data.length;i++){
-            console.log(response.data[i].fullName)
-            console.log(response.data[i].description)
-            console.log(response.data[i].url)
-            console.log(response.data[i].images[0].url)
+            console.log(response.data[i].name)
+            console.log(response.data[i].reservationUrl)
+            console.log(response.data[i].fees)
             //.parkarea is where the dynamic data gets appended to
         }
         
     });
 }
+
+function weather(parkweather){
+    var url=""
+    console.log(url)
+    $.ajax({
+        url: url,
+        method: "GET"
+    })
+    .then(function(response)
+    {
+        for(var i=0 ;i<response.data.lenght;i++){
+            console.log(response.)
+            console.log(0)
+            console.log()
+        }
+    });
+
+}
 parks("FL")
+campground("")
+weather()
